9354c9e80e3e613fa87096f53828ac433693472d
mlogly-eng <mlogly@gmail.com>
2026-03-06 12:31:23 +0200

    migrate to Next.js pages

--- app/ophtho/page.js
+++ app/ophtho/page.js
@@ -1,7 +1,7 @@
-export default function Home() {
+export default function Ophtho() {
   return (
     <iframe 
-      src="/index.html" 
+      src="/vent-ophtho.html" 
       style={{width:'100%', height:'100vh', border:'none'}}
     />
   )
--- app/qbank/page.js
+++ app/qbank/page.js
@@ -1,7 +1,7 @@
-export default function Home() {
+export default function Qbank() {
   return (
     <iframe 
-      src="/index.html" 
+      src="/vent-qbank.html" 
       style={{width:'100%', height:'100vh', border:'none'}}
     />
   )
